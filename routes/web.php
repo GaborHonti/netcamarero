34775289b1583b36c97d7e49bfd52c2815696751
unknown <ghonti67@gmail.com>
2020-06-07 15:36:11 +0200

    cambiado homepage

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome.blade.php');
+    return view('welcome');
 });
 /*
 Route::get('/crudmenu', function () {
